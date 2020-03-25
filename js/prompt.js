66d86190982295ea4d9f06994db32ee9081845a2
victorAnumudu <anumuduchukwuebuka@gmail.com>
2020-03-25 21:34:00 +0100

    first push of thriveagriv

---
+++ js/prompt.js
@@ -0,0 +1,8 @@
+// var x = prompt("enter your surname");
+// var y = prompt("enter your firstname");
+// console.log("your name is " + x + " "+ y); 
+
+/*var x = Number(prompt("enter first number"));
+var y = Number(prompt("enter second number"));
+var c = (x + y);
+console.log(c);*/
